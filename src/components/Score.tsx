449006eef88b0f2f4c1c8a188f091b3a706719df
Paulina Nilsson <67747928+Paulina97@users.noreply.github.com>
2021-07-18 23:49:53 +0200

    implement Score

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,10 +1,29 @@
 import '../scss/score.scss'
 
+type ScoreProps = {
+  moves: number
+  bestScore: number
+}
 
 function Score(props:ScoreProps) {
 
   return (
     <div className="score-container">
+      <div className="score">
+        <div>
+          <span>Moves:</span> {props.moves}
+        </div>
+        {props.bestScore && (
+          <div>
+            <span>Best score:</span> {props.bestScore}
+          </div>
+        )}
+      </div>
+      <div>
+        <button onClick={() => {window.location.reload()}}>
+          RESTART
+        </button>
+      </div>
     </div>
   )
 }
