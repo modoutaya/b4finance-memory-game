d36d239e2b88b5f56e97020d910abc5c078c16fd
Paulina Nilsson <67747928+Paulina97@users.noreply.github.com>
2021-07-18 23:50:11 +0200

    implement App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
 import Score from './Score'
 import Board from './Board'
+import { useState } from 'react'
 
+
+const cardIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+cardIds.sort(() => 0.5 - Math.random())
+console.log(cardIds);
 
 function App() {
 
+  const [moves, setMoves] = useState<number>(0)
+  const [bestScore, setBestScore] = useState<number>(
+    parseInt(localStorage.getItem('bestScore') || '0')
+  )
+  const finishGameCallback = () => {
+    const newBestScore = moves < bestScore ? moves : bestScore
+    setBestScore(newBestScore)
+    localStorage.setItem('bestScore', '' + newBestScore)
+  }
+
   return (
     <div className="app-container">
-      <Score/>
-      <Board/>
+      <Score 
+        moves={moves} 
+        bestScore={bestScore}
+      />
+      <Board 
+        setMoves={setMoves} 
+        finishGameCallback={finishGameCallback} 
+        cardIds={cardIds}
+      />
     </div>
   )
 }
