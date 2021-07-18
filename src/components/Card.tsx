c24eff8527fa7fc0c451d0edf0f40038bdbbfad4
Paulina Nilsson <67747928+Paulina97@users.noreply.github.com>
2021-07-18 23:49:16 +0200

    implement Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,10 +2,37 @@ import classnames from 'classnames'
 import '../scss/card.scss'
 
 
+type CardProps = {
+  image: string,
+  onClick: (id: number) => void,
+  id: number,
+  isInactive: boolean,
+  isFlipped: boolean,
+  isDisabled: boolean
+}
+
 function Card(props: CardProps) {
 
+  const backSide = '/images/backside.png'
+
+  const handleClick = () => {
+    !props.isFlipped && !props.isDisabled && props.onClick(props.id);
+  };
+
   return (
-    <div>
+    <div
+      className={classnames("card", {
+        "is-flipped": props.isFlipped,
+        "is-inactive": props.isInactive
+      })}
+      onClick={handleClick}
+    >
+      <div className="card-face">
+        <img src={backSide} alt="card backside" />
+      </div>
+      <div className="card-face card-back-face">
+        <img src={props.image} alt="card" />
+      </div>
     </div>
   )
 }
